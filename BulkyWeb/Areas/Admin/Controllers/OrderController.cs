8b1f8287c422d8168daefceee80d86b15ede3d6c
dan@dopksi <hextra@live.com>
2023-09-25 12:38:29 +0100

    Added a sleek theme

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -85,7 +85,7 @@ namespace ProductSolution.web.Areas.Admin.Controllers
             orderHeader.ShippingDate = DateTime.Now;
             if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
             {
-                orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+                orderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
             }
             _unitOfWork.OrderHeader.Update(orderHeader);
             _unitOfWork.Save();
